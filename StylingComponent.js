09a13d76554a8079593d3c5c0e9869d584a5f82b
Zoya Jetham <92311895+Zooiiee@users.noreply.github.com>
2024-12-19 01:42:37 +0530

    Create StylingComponent.js

---
+++ StylingComponent.js
@@ -0,0 +1,30 @@
+//app1.js
+
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <body bgcolor="pink">
+      <h1><i>Hello World!</i></h1>
+      <p><i>Zoieeeeee!❤️❤️</i></p>
+      </body>
+    </div>
+  );
+}
+
+
+export default App;
+
+
+
+//App.css
+
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+
